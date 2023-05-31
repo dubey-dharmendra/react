b2b70fffc0620d17ea6cc81fc05b40a31fc9a700
swagger47 <dharmendradubey483@gmail.com>
2023-05-31 15:11:06 +0530

    basic of react 1st episode

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "this is child 1 h1 tag"),
+    React.createElement("h2", {}, "this is child 1 h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "this is child 2 h1 tag"),
+    React.createElement("h2", {}, "this is child 2 h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
